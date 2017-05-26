f4631b8df261374fa97afdbdd33e28f6e1ed8222
Ivan Notaros <nothke@gmail.com>
2017-05-26 16:30:55 +0200

    Cleanup

--- Assets/DPhysics/Editor/Vector3dDrawer.cs
+++ Assets/DPhysics/Editor/Vector3dDrawer.cs
@@ -4,22 +4,10 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(Vector3d))]
 public class Vector3dDrawer : PropertyDrawer
 {
-    // Draw the property inside the given rect
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // Using BeginProperty / EndProperty on the parent property means that
-        // prefab override logic works on the entire property.
         EditorGUI.BeginProperty(position, label, property);
 
-        // Draw label
-
-        // Don't make child fields be indented
-        //var indent = EditorGUI.indentLevel;
-        //EditorGUI.indentLevel = 0;
-
-        // Calculate rects
-
-        
         float ldiv = 14;
 
         EditorGUIUtility.labelWidth = position.width / 4;
@@ -43,19 +31,10 @@ public class Vector3dDrawer : PropertyDrawer
         var zRect = new Rect(curX, position.y, sep - 2, position.height);
         curX += sep;
 
-        // Draw fields - passs GUIContent.none to each so they are drawn without labels
         EditorGUIUtility.labelWidth = ldiv;
-        //EditorGUIUtility.fieldWidth = sep - ldiv;
         EditorGUI.PropertyField(xRect, property.FindPropertyRelative("x"), new GUIContent("X"));
         EditorGUI.PropertyField(yRect, property.FindPropertyRelative("y"), new GUIContent("Y"));
         EditorGUI.PropertyField(zRect, property.FindPropertyRelative("z"), new GUIContent("Z"));
-
-        //EditorGUI.DoubleField(xRect, "X", property.FindPropertyRelative("x").doubleValue, );
-        //EditorGUI.DoubleField(yRect, "Y", property.FindPropertyRelative("x").doubleValue);
-        //EditorGUI.DoubleField(zRect, "Z", property.FindPropertyRelative("x").doubleValue);
-
-        // Set indent back to what it was
-        //EditorGUI.indentLevel = indent;
 
         EditorGUI.EndProperty();
     }
